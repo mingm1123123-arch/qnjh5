527be63e52198255b66d8cf53e9d396fae8b6bc8
mingming <mingm1123123@gmail.com>
2026-05-17 18:42:44 +0800

    Revert "feat(router): 添加路由配置和SEO优化功能"
    This reverts commit 2e145099139de4f21c5a933428f5aee99213dd8e.

--- src/router/index.js
+++ src/router/index.js
@@ -5,23 +5,12 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history', // 使用history模式，URL更友好，利于SEO
-  scrollBehavior(to, from, savedPosition) {
-    // 滚动行为控制
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return { x: 0, y: 0 }
-    }
-  },
   routes: [
     {
       path: '/loading',
       name: 'loading',
       meta: {
-        title: '加载... | 睿芯云汇',
-        description: '加载中...',
-        keywords: '睿芯云汇,加载'
+        title: '加载...'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/loading.vue')
     },
@@ -29,9 +18,7 @@ export default new Router({
       path: '/',
       name: 'downloadApp',
       meta: {
-        title: '睿芯云汇APP下载 - 官方正版',
-        description: '睿芯云汇APP官方下载页面，提供安卓最新版本下载，安全无毒，请放心使用。',
-        keywords: '睿芯云汇下载,APP下载,安卓版下载'
+        title: '睿芯云汇'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/downloadApp.vue')
     },
@@ -39,9 +26,7 @@ export default new Router({
       path: '/aloneHome',
       name: 'aloneHome',
       meta: {
-        title: '个人感悟 - 睿芯云汇',
-        description: '查看用户的个人感悟分享',
-        keywords: '个人感悟,感悟分享'
+        title: '个人感悟'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/aloneHome.vue')
     },
@@ -49,9 +34,7 @@ export default new Router({
       path: '/details',
       name: 'details',
       meta: {
-        title: '感悟详情 - 睿芯云汇',
-        description: '查看详细感悟内容',
-        keywords: '感悟详情,文章内容'
+        title: '感悟详情'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/details.vue')
     },
@@ -59,9 +42,7 @@ export default new Router({
       path: '/news',
       name: 'news',
       meta: {
-        title: '消息中心 - 睿芯云汇',
-        description: '查看最新通知和消息',
-        keywords: '消息,通知'
+        title: '新的消息'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/news.vue')
     },
@@ -69,9 +50,7 @@ export default new Router({
       path: '/pub',
       name: 'pub',
       meta: {
-        title: '发布感悟 - 睿芯云汇',
-        description: '发布您的个人感悟和想法',
-        keywords: '发布,写感悟'
+        title: '发布'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/pub.vue')
     },
@@ -79,9 +58,7 @@ export default new Router({
       path: '/follow',
       name: 'follow',
       meta: {
-        title: '我的关注 - 睿芯云汇',
-        description: '查看我关注的用户列表',
-        keywords: '关注,粉丝'
+        title: '关注'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/follow.vue')
     },
@@ -89,9 +66,7 @@ export default new Router({
       path: '/fans',
       name: 'fans',
       meta: {
-        title: '我的粉丝 - 睿芯云汇',
-        description: '查看我的粉丝列表',
-        keywords: '粉丝,关注者'
+        title: '粉丝'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/fans.vue')
     },
@@ -99,9 +74,7 @@ export default new Router({
       path: '/friend',
       name: 'friend',
       meta: {
-        title: '好友列表 - 睿芯云汇',
-        description: '查看好友列表',
-        keywords: '好友,朋友'
+        title: '关注'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/friend.vue')
     },
@@ -109,9 +82,7 @@ export default new Router({
       path: '/richText',
       name: 'richText',
       meta: {
-        title: '详情 - 睿芯云汇',
-        description: '查看详细内容',
-        keywords: '详情,内容'
+        title: '详情'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/richText.vue')
     },
@@ -119,9 +90,7 @@ export default new Router({
       path: '/privacyPolicy',
       name: 'privacyPolicy',
       meta: {
-        title: '隐私政策 - 睿芯云汇',
-        description: '睿芯云汇隐私政策，保护用户隐私安全',
-        keywords: '隐私政策,用户协议'
+        title: '隐私政策'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/privacyPolicy.vue')
     },
@@ -129,9 +98,7 @@ export default new Router({
       path: '/agreement',
       name: 'agreement',
       meta: {
-        title: '用户协议 - 睿芯云汇',
-        description: '睿芯云汇用户服务协议',
-        keywords: '用户协议,服务条款'
+        title: '用户协议'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/agreement.vue')
     },
@@ -139,9 +106,7 @@ export default new Router({
       path: '/courseDetails',
       name: 'courseDetails',
       meta: {
-        title: '课程评论 - 睿芯云汇',
-        description: '查看课程评论和学习心得',
-        keywords: '课程,评论,学习'
+        title: '课程评论'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/courseDetails.vue')
     },
@@ -149,9 +114,7 @@ export default new Router({
       path: '/shop',
       name: 'shop',
       meta: {
-        title: '积分商城 - 睿芯云汇',
-        description: '使用积分兑换精美商品，礼品丰富多样',
-        keywords: '积分商城,积分兑换,礼品'
+        title: '积分商城'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/scoreShop/shop.vue')
     },
@@ -159,9 +122,7 @@ export default new Router({
       path: '/goodsDetail',
       name: 'goodsDetail',
       meta: {
-        title: '商品详情 - 积分商城 - 睿芯云汇',
-        description: '查看商品详细信息并进行积分兑换',
-        keywords: '商品详情,积分兑换'
+        title: '商品详情'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/scoreShop/goodsDetail.vue')
     },
@@ -169,9 +130,7 @@ export default new Router({
       path: '/buyOrder',
       name: 'buyOrder',
       meta: {
-        title: '兑换记录 - 积分商城 - 睿芯云汇',
-        description: '查看您的积分兑换订单记录',
-        keywords: '兑换记录,订单'
+        title: '兑换列表'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/scoreShop/buyOrder.vue')
     },
@@ -179,9 +138,7 @@ export default new Router({
       path: '/messageList',
       name: 'messageList',
       meta: {
-        title: '新闻资讯 - 睿芯云汇',
-        description: '查看最新新闻资讯',
-        keywords: '新闻,资讯'
+        title: '新闻'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/messageList.vue')
     },
@@ -189,9 +146,7 @@ export default new Router({
       path: '/userManager',
       name: 'userManager',
       meta: {
-        title: '客服管理 - 睿芯云汇',
-        description: '客服操作界面',
-        keywords: '客服管理'
+        title: '客服操作界面'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/serve/userManager.vue')
     },
@@ -199,9 +154,7 @@ export default new Router({
       path: '/userDetail',
       name: 'userDetail',
       meta: {
-        title: '用户详情 - 睿芯云汇',
-        description: '查看用户详细信息',
-        keywords: '用户详情'
+        title: '用户详情'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/serve/userDetail.vue')
     },
@@ -209,9 +162,7 @@ export default new Router({
       path: '/loginLogList',
       name: 'loginLogList',
       meta: {
-        title: '登录记录 - 睿芯云汇',
-        description: '查看用户登录记录',
-        keywords: '登录记录'
+        title: '登录记录'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/serve/loginLogList.vue')
     },
@@ -219,9 +170,7 @@ export default new Router({
       path: '/liveLog',
       name: 'liveLog',
       meta: {
-        title: '直播记录 - 睿芯云汇',
-        description: '查看直播历史记录',
-        keywords: '直播记录'
+        title: '直播记录'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/serve/liveLog.vue')
     },
@@ -229,9 +178,7 @@ export default new Router({
       path: '/goodLog',
       name: 'goodLog',
       meta: {
-        title: '兑换记录 - 睿芯云汇',
-        description: '查看商品兑换记录',
-        keywords: '兑换记录'
+        title: '商品兑换'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/serve/goodLog.vue')
     },
@@ -239,9 +186,7 @@ export default new Router({
       path: '/userList',
       name: 'userList',
       meta: {
-        title: '用户列表 - 睿芯云汇',
-        description: '查看所有用户列表',
-        keywords: '用户列表'
+        title: '用户列表'
       },
       component: () => import(/* webpackChunkName: "about" */'@/views/serve/userList.vue')
     }
